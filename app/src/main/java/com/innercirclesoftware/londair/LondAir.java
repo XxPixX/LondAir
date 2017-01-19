a3279301f50d6ca681cadfd503953cdb4a07e5a6
Maciej Kaznowski <mmkaznowski@gmail.com>
2017-01-19 18:56:42 +0000

    Use custom Application object

---
+++ app/src/main/java/com/innercirclesoftware/londair/LondAir.java
@@ -0,0 +1,11 @@
+package com.innercirclesoftware.londair;
+
+import android.app.Application;
+
+public class LondAir extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
