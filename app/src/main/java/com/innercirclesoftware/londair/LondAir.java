22cfffc9b6aaa2f9d1559737f73f88f0ac245731
Maciej Kaznowski <mmkaznowski@gmail.com>
2017-01-19 18:58:23 +0000

    Add Timber logging library

--- app/src/main/java/com/innercirclesoftware/londair/LondAir.java
+++ app/src/main/java/com/innercirclesoftware/londair/LondAir.java
@@ -2,10 +2,17 @@ package com.innercirclesoftware.londair;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class LondAir extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initTimber();
+    }
+
+    private void initTimber() {
+        Timber.plant(new Timber.DebugTree());
     }
 }
